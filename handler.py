85c65e3bbe0fbbaf90e4d9dc531f2bdfd26ef4c0
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-03-22 23:59:14 +0400

    Fourth commit

--- handler.py
+++ handler.py
@@ -1,52 +1,55 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from random import randrange
+from auth import group_auth
+
+group_session = vk_api.VkApi(token=group_auth)
+longpoll = VkLongPoll(group_session)
 
 
-class Handler:
-    def __init__(self, group_auth):
-        self.group_session = vk_api.VkApi(token=group_auth)
-        self.longpoll = VkLongPoll(self.group_session)
+def listener():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            client = event.user_id
+            text = event.text.lower()
+            return client, text
 
-    def listener(self):
-        for event in self.longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                client = event.user_id
-                text = event.text.lower()
-                return client, text
 
-    def new_message(self, user_id, message, keyboard=None):
-        params = {
-            'user_id': user_id,
-            'message': message,
-            'random_id': randrange(10 ** 7)
-        }
-        if keyboard is not None:
-            params['keyboard'] = keyboard.get_keyboard()
-        else:
-            params = params
-        self.group_session.method('messages.send', params)
+def new_message(user_id, message, keyboard=None):
+    params = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': randrange(10 ** 7)
+    }
+    if keyboard is not None:
+        params['keyboard'] = keyboard.get_keyboard()
+    else:
+        params = params
+    group_session.method('messages.send', params)
 
-    def search_candidates(self, user_id, user_auth, search_params):
-        user_session = vk_api.VkApi(token=user_auth)
-        params = {
-            'count': 20,
-            'fields': 'screen_name',
-            'has_photo': 1
-        }
-        response = user_session.method('users.search', {**search_params, **params})
-        items = [item for item in response['items'] if not item['is_closed']]
-        for candidate_id in items:
-            self.photos_get(user_id, (candidate_id['id']), user_session, user_auth)
 
-    def photos_get(self, user_id, owner_id, user_session, user_auth):
+def send_photo(user_id, owner_id, photo_id):
+    attachment = f'photo{owner_id}_{photo_id}_{group_auth}'
+    send_photo_params = {
+        'user_id': user_id,
+        'attachment': attachment,
+        'random_id': randrange(10 ** 7)
+    }
+    group_session.method('messages.send', {**send_photo_params})
+
+
+class GetMatches:
+    def __init__(self, user_auth):
+        self.session = vk_api.VkApi(token=user_auth)
+
+    def get_photos(self, user_id, owner_id):
         photos_get_params = {
             'owner_id': owner_id,
             'album_id': 'profile',
             'extended': 1,
             'count': 3
         }
-        response = user_session.method('photos.get', {**photos_get_params})
+        response = self.session.method('photos.get', {**photos_get_params})
         items = response['items']
 
         def likes_and_comments_count(item):
@@ -55,14 +58,15 @@ class Handler:
         items.sort(key=likes_and_comments_count)
         most_liked_photos = items[-3:]
         for photo in most_liked_photos:
-            self.send_photo(user_id, photo['owner_id'], photo['id'], user_auth, user_session)
+            send_photo(user_id, photo['owner_id'], photo['id'])
 
-    def send_photo(self, user_id, owner_id, photo_id, user_auth, user_session):
-        attachment = f'photo{owner_id}_{photo_id}_{user_auth}'
-        send_photo_params = {
-            'user_id': user_id,
-            'attachment': attachment,
-            'random_id': randrange(10 ** 7)
+    def search_candidates(self, user_id, search_params):
+        params = {
+            'count': 20,
+            'fields': 'screen_name',
+            'has_photo': 1
         }
-        user_session.method('messages.send', {**send_photo_params})
-
+        response = self.session.method('users.search', {**search_params, **params})
+        items = [item for item in response['items'] if not item['is_closed']]
+        for candidate_id in items:
+            self.get_photos(user_id, (candidate_id['id']))
--- main.py
+++ main.py
@@ -1,68 +1,67 @@
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import re
-from auth import group_auth
-from handler import Handler
+from handler import GetMatches, new_message, listener
 
 
 class User:
     def __init__(self):
         self.search_params = {}
 
-    def token_param(self, user_id, session):
-        session.new_message(user_id, """
+    def token_param(self, user_id):
+        new_message(user_id, """
             И самое главное - чтобы запустить поиск, нам нужен твой персональный токен! 
             """)
-        text = session.listener()[1]
+        text = listener()[1]
         if re.match(r'[a-z0-9]*', text):
             token = re.match(r'[a-z0-9]*', text)
             user_auth = token[0]
-            session.new_message(user_id, """
+            new_message(user_id, """
                         Начинаем поиск! 
                         """)
-            session.search_candidates(user_id, user_auth, self.search_params)
+            seeker = GetMatches(user_auth)
+            seeker.search_candidates(user_id, self.search_params)
 
-    def city_param(self, user_id, session):
-        session.new_message(user_id, """
+    def city_param(self, user_id):
+        new_message(user_id, """
             А в каком городе мы ведем поиск?
             """)
-        text = session.listener()[1]
+        text = listener()[1]
         if re.match(r'[a-zа-я]*', text):
             home_town = re.match(r'[a-zа-я]*', text)
             self.search_params['home_town'] = home_town[0]
-            self.token_param(user_id, session)
+            self.token_param(user_id)
 
-    def age_param(self, user_id, session):
-        session.new_message(user_id, """
+    def age_param(self, user_id):
+        new_message(user_id, """
             Прекрасный выбор! Давай сузим круг поиска, укажи возраст в формате "от" и "до"
             """)
-        text = session.listener()[1]
+        text = listener()[1]
         if re.findall(r'(?<!\d)\d{2}(?!\d)', text):
             age_from = int(re.findall(r'(?<!\d)\d{2}(?!\d)', text)[0])
             age_to = int(re.findall(r'(?<!\d)\d{2}(?!\d)', text)[1])
             self.search_params['age_from'] = age_from
             self.search_params['age_to'] = age_to
-            self.city_param(user_id, session)
+            self.city_param(user_id)
 
-    def sex_param(self, user_id, session):
-        text = session.listener()[1]
+    def sex_param(self, user_id):
+        text = listener()[1]
         if text == 'старт!':
-            session.new_message(user_id, """
+            new_message(user_id, """
                 Супер! Сначала определимся кого мы ищем - для этого:
                 нажми 1, если ищешь девушку, 2 - если парня, и 0 - если это неважно
                 """)
-            text = session.listener()[1]
+            text = listener()[1]
             if re.findall(r'(?<!\d)\d(?!\d)', text):
                 self.search_params['sex'] = text
-                self.age_param(user_id, session)
+                self.age_param(user_id)
 
     def get_started(self):
-        session = Handler(group_auth)
-        user_id = session.listener()[0]
-        session.new_message(user_id, "Привет! Это VKinder - лучший сервис для знакомств!")
+        user_id = listener()[0]
+        new_message(user_id, "Привет! Это VKinder - лучший сервис для знакомств!")
         keyboard = VkKeyboard(one_time=True)
         keyboard.add_button("Старт!", VkKeyboardColor.PRIMARY)
-        session.new_message(user_id, 'Нажми Старт!, чтобы начать поиск!', keyboard)
-        self.sex_param(user_id, session)
+        new_message(user_id, 'Нажми Старт!, чтобы начать поиск!', keyboard)
+        self.sex_param(user_id)
 
 
 if __name__ == '__main__':
